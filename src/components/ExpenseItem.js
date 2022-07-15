e8184f864306a187fc80f2eed5549bfdea9ea927
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2022-07-15 17:15:50 +0530

    handle input / event.target.value

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,25 @@
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css'
+import { useState } from 'react';
 
 let ExpenseItem = (props) => {
-
+    let [input, setInput] = useState("hi");
+    let inp = (event) => {
+        setInput(event.target.value);
+    }
+    var [title, setTitle] = useState(props.title);
     let but = () => {
-        alert("clicked");
+         setTitle(input);
     }
     return (
       <Card className='expense-item'>
       <ExpenseDate  date = {props.date}/>
       <div className='expense-des'>
-        <h2>{props.title}</h2>
+        <h2 id='title'>{title}</h2>
         <div className='price'>${props.price}</div>
       </div> 
+      <input type="text" value={input} onChange={inp}></input>
       <button onClick={but} className='but'>Change Title</button>
     </Card>
     )
